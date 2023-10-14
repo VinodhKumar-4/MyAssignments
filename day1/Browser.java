938340837112c1fd32e42bea60e9b2243bfd176d
VinodhKumar-4 <147938900+VinodhKumar-4@users.noreply.github.com>
2023-10-14 22:26:32 +0530

    Add files via upload
    day1

---
+++ day1/Browser.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Browser {
+	public static void main(String[] args) {
+		float browserVersion = 30.2f;
+		String browserName = "Opera mini";
+		boolean pageLoaded = true;
+		int noOfBrowser = 4;
+		char browserLogo = 'O';
+		System.out.println(browserVersion);
+		System.out.println(browserName);
+		System.out.println(pageLoaded);
+		System.out.println(noOfBrowser);
+		System.out.println(browserLogo);
+		
+		
+		
+		
+	}
+}
---
+++ day1/findnumber.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class findnumber {
+
+	public static void main(String[] args) {
+		int number = -5;
+		if(number>0)
+		{
+			System.out.println("Number is positive");
+		}
+		else
+		{
+			System.out.println("Number is negative");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
